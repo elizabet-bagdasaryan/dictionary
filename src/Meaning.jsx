ff487b3dad6d56c91bfb32985acb28ede22b2c43
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-05-18 10:43:00 +0400

    wip: build the ui

---
+++ src/Meaning.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Meanings() {
+  return (
+    <>
+      <div className="p-4">
+        <div className="my-3">
+          <div className="flex flex-row items-center text-lg font-bold font-serif">
+            <p>Noun</p>
+            <hr className="w-full ml-100"></hr>
+          </div>
+          <p className="text-gray-800 mt-3 font-semibold">Meaning</p>
+          <ul className="list-disc px-10 text-gray-800 text-sm space-y-2">
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem,
+              rem harum dolore nemo et vitae cum laudantium facere ratione
+              alias, est veritatis deleniti, nesciunt impedit quam. Nobis iste
+              dolores vitae.
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem,
+              rem harum dolore nemo et vitae cum laudantium facere ratione
+              alias, est veritatis deleniti, nesciunt impedit quam. Nobis iste
+              dolores vitae.
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem,
+              rem harum dolore nemo et vitae cum laudantium facere ratione
+              alias, est veritatis deleniti, nesciunt impedit quam. Nobis iste
+              dolores vitae.
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem,
+              rem harum dolore nemo et vitae cum laudantium facere ratione
+              alias, est veritatis deleniti, nesciunt impedit quam. Nobis iste
+              dolores vitae.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Meanings;
